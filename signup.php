ffa87363d2caeaebe5736c4f77b1738a700ca771
mattiasbitar <mattiasbitar@hotmail.se>
2017-02-18 11:29:00 +0100

    Login/Signup - Backend focus

---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php
+	
+	include 'DBhandler.php';
+	
+	$userName = $_POST['userName'];
+	$password = $_POST['password'];
+	
+
+	$sql = "INSERT INTO UserTable(userName, password) VALUES ('$userName', '$password')";
+	$result = $conn->query($sql);
+	
+	header("Location: index.php");
